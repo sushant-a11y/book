97491488d84323663b845e75583fb9958fea033c
Sushant Indupuru <si6854@pleasantonusd.net>
2024-11-20 17:07:58 +0000

    more fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-
-
 class Main {
     public static void main(String[] args) throws java.io.IOException, java.net.URISyntaxException {
         Book book1 = new Book("https://www.gutenberg.org/cache/epub/11/pg11.txt");
